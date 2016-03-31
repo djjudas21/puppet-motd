fa56c96c75dec8cc5b611193919a6d475f978cd1
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-03-31 15:01:09 +0100

    Trivial fixes to pass lint checking

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,14 +20,14 @@ class motd (
   # Check we're only using a single character to draw the box
   validate_slength($char, 1, 1)
 
-  $boxwidth = $width-2
-  $printwidth = $boxwidth-8
+  $boxwidth = $width - 2
+  $printwidth = $boxwidth - 8
 
   # Calculate a blank line, i.e. two chars with no text
   $blank = inline_template("  <%= @char %><% for x in (3..boxwidth) %> <% end%><%= @char %>\n")
 
   # Calculate a top or bottom edge row of chars
-  $edge = inline_template("  <% for x in (1..boxwidth) %><%= @char %><% end %>")
+  $edge = inline_template('  <% for x in (1..boxwidth) %><%= @char %><% end %>')
 
   # Location of motd file
   concat { $path:
